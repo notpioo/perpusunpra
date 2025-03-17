fa6f6b365023106f2cf5e97dafdef958b6d560d8
Avionika <afio12327@gmail.com>
2025-03-17 08:07:56 +0700

    p

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,8 +1,23 @@
+
 import gulp from 'gulp';
 import open from 'gulp-open';
 
 // Task to open the browser
-gulp.task('open-app', function(){
-  gulp.src('pages/dashboard.html')
-  .pipe(open());
+gulp.task('open-app', () => {
+  return gulp.src('pages/dashboard.html')
+    .pipe(open({uri: 'http://0.0.0.0:3000/pages/dashboard.html'}));
 });
+
+// Add server task
+gulp.task('serve', () => {
+  return import('gulp-connect').then((connect) => {
+    connect.default.server({
+      root: './',
+      port: 3000,
+      host: '0.0.0.0'
+    });
+  });
+});
+
+// Default task
+gulp.task('default', gulp.series('serve', 'open-app'));
